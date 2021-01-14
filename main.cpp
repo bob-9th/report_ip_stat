c45a090e40859a239be62615ecab3f75cd23c955
nnnlog <sorisem4106@naver.com>
2021-01-14 09:09:32 +0900

    string 변환 과정 삭제와 바이너리 데이터를 키로 사용

--- main.cpp
+++ main.cpp
@@ -10,7 +10,25 @@ using namespace std;
 struct A {
 	int send, recv;
 	int sB, rB;
+
+	bool operator<(const A &b) const {
+		if (send != b.send) return send < b.send;
+		if (recv != b.recv) return recv < b.recv;
+		if (sB != b.sB) return sB < b.sB;
+		if (rB != b.rB) return rB < b.rB;
+		return 0;
+	}
 };
+
+bool operator<(const ether_addr &a, const ether_addr &b) {
+	bool f = false;
+	for (int i = 0; i < ETH_ALEN; i++)
+		if (a.ether_addr_octet[i] != b.ether_addr_octet[i]) {
+			f = a.ether_addr_octet[i] < b.ether_addr_octet[i];
+			break;
+		}
+	return f;
+}
 
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
@@ -32,8 +50,22 @@ int main(int argc, char *argv[]) {
 		return -0;
 	}
 
-	unordered_map<string, A> mp; //ip
-	unordered_map<string, A> mac_mp; //mac address
+	unordered_map<uint32_t, A> mp; //ip
+	set<pair<ether_addr, A>> mac_mp; //mac address
+
+	auto update = [](auto &mac_mp, const ether_addr &mac, bool isRecv, int byteLen) {
+		auto it = mac_mp.lower_bound(make_pair(mac, A{}));
+		A val = {};
+		if (it != mac_mp.end()) val = it->second, mac_mp.erase(it);
+		if (isRecv) {
+			++val.recv;
+			val.rB += byteLen;
+		} else {
+			++val.send;
+			val.sB += byteLen;
+		}
+		mac_mp.insert(make_pair(mac, val));
+	};
 
 	while (1) {
 		pcap_pkthdr *header;
@@ -47,41 +79,36 @@ int main(int argc, char *argv[]) {
 		if (res == -2) break;
 
 		auto *eth = (ether_header *) packet;
-		string src_mac = string(ether_ntoa((ether_addr *) eth->ether_shost)),
-				dst_mac = string(ether_ntoa((ether_addr *) eth->ether_dhost));
 
-		auto &mac_dst = mac_mp[dst_mac];
-		++mac_dst.recv;
-		mac_dst.rB += header->caplen;
-
-		auto &mac_src = mac_mp[src_mac];
-		++mac_src.send;
-		mac_src.sB += header->caplen;
+		ether_addr tmp{};
+		memcpy(tmp.ether_addr_octet, eth->ether_dhost, sizeof(eth->ether_dhost));
+		update(mac_mp, tmp, true, header->caplen);
+		memcpy(tmp.ether_addr_octet, eth->ether_shost, sizeof(eth->ether_shost));
+		update(mac_mp, tmp, false, header->caplen);
 
 		ip *_ip = (ip *) (packet + 14);
 		if (ntohs(eth->ether_type) == ETHERTYPE_IP) {
-			string src_ip = string(inet_ntoa(_ip->ip_src));
-			string dst_ip = string(inet_ntoa(_ip->ip_dst));
-
-			auto &dst = mp[dst_ip];
+			auto &dst = mp[_ip->ip_src.s_addr];
 			++dst.recv;
 			dst.rB += header->caplen;
 
-			auto &src = mp[src_ip];
+			auto &src = mp[_ip->ip_dst.s_addr];
 			++src.send;
 			src.sB += header->caplen;
 		}
 	}
 
 	for (const auto &[ip, info] : mp)
-		cout << ip << " (total " << info.rB + info.sB << " bytes)\n received " << info.recv << " ip packets ("
+		cout << inet_ntoa({ip}) << " (total " << info.rB + info.sB << " bytes)\n received " << info.recv
+		     << " ip packets ("
 		     << info.rB << " bytes)\n "
 		     << "sent " << info.send << " ip packets (" << info.sB << " bytes)\n\n";
 
 	for (const auto &[mac, info] : mac_mp)
-		cout << mac << " (total " << info.rB + info.sB << " bytes)\n received " << info.recv << " mac packets ("
+		cout << ether_ntoa(&mac) << " (total " << info.rB + info.sB << " bytes)\n received " << info.recv
+		     << " ethernet packets ("
 		     << info.rB << " bytes)\n "
-		     << "sent " << info.send << " mac packets (" << info.sB << " bytes)\n\n";
+		     << "sent " << info.send << " ethernet packets (" << info.sB << " bytes)\n\n";
 
 
 	pcap_close(handle);
