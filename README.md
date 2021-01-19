e7c08c4cdbfab778f7dfa58b20d02bd9c5eb7e3c
nnnlog <sorisem4106@naver.com>
2021-01-19 21:18:46 +0900

    tcp/ethernet conversation 추가

--- README.md
+++ README.md
@@ -13,16 +13,16 @@ make
 ## Output
 ```
 175.213.35.39 (total 1255 bytes)
- received 6 ip packets (546 bytes)
- sent 4 ip packets (709 bytes)
-
-10.2.2.3 (total 1571 bytes)
- received 6 ip packets (875 bytes)
- sent 8 ip packets (696 bytes)
+ received 4 ip packets (709 bytes)
+ sent 6 ip packets (546 bytes)
 
 10.2.2.1 (total 316 bytes)
- received 2 ip packets (150 bytes)
- sent 2 ip packets (166 bytes)
+ received 2 ip packets (166 bytes)
+ sent 2 ip packets (150 bytes)
+
+10.2.2.3 (total 1571 bytes)
+ received 8 ip packets (696 bytes)
+ sent 6 ip packets (875 bytes)
 
 88:c3:97:c7:1b:5 (total 1571 bytes)
  received 8 ethernet packets (696 bytes)
@@ -31,4 +31,12 @@ make
 b4:2e:99:ea:97:de (total 1571 bytes)
  received 6 ethernet packets (875 bytes)
  sent 8 ethernet packets (696 bytes)
+
+=== DUMP TCP Conversations ===
+A -> B     Send(Count/Len) / Recv(Count/Len)
+10.2.2.3:48326 -> 175.213.35.39:80    >    6 packets 546 bytes / 4 packets 709 bytes
+
+=== DUMP Ethernet Conversations ===
+A -> B     Send(Count/Len) / Recv(Count/Len)
+88:c3:97:c7:1b:5 -> b4:2e:99:ea:97:de    >    6 packets 875 bytes / 8 packets 696 bytes
 ```
--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 #include <pcap.h>
 #include <netinet/ip.h>
 #include <netinet/ether.h>
-#include <netinet/in.h>
-#include <net/ethernet.h>
+#include <netinet/tcp.h>
 
 using namespace std;
+
+typedef pair<uint32_t, uint16_t> tcp_key;
 
 struct A {
 	int send, recv;
@@ -39,14 +40,14 @@ int main(int argc, char *argv[]) {
 	pcap_t *handle = pcap_fopen_offline(f, errbuf);
 	if (handle == nullptr) {
 		cerr << "pcap_fopen_offline(" << argv[1] << ") return nullptr - " << errbuf << "\n";
-		return -0;
+		return 0;
 	}
 
 	unordered_map<uint32_t, A> mp; //ip
 	map<ether_addr, A> mac_mp; //mac address
 
-	auto update = [](auto &mac_mp, const auto &key, bool isRecv, int byteLen) {
-		A &val = mac_mp[key];
+	auto update = [](auto &endpoint, const auto &key, bool isRecv, int byteLen) {
+		A &val = endpoint[key];
 		if (isRecv) {
 			++val.recv;
 			val.rB += byteLen;
@@ -54,6 +55,14 @@ int main(int argc, char *argv[]) {
 			++val.send;
 			val.sB += byteLen;
 		}
+	};
+
+	map<tcp_key, map<tcp_key, pair<int, int>>> conv_tcp; //tcp conversation
+	map<ether_addr, map<ether_addr, pair<int, int>>> conv_ether; //ethernet conversation
+
+	auto flow_update = [](auto &conversation, const auto &src, const auto &dst, int byteLen) {
+		++conversation[src][dst].first;
+		conversation[src][dst].second += byteLen;
 	};
 
 	while (1) {
@@ -69,16 +78,29 @@ int main(int argc, char *argv[]) {
 
 		auto *eth = (ether_header *) packet;
 
-		ether_addr tmp{};
-		memcpy(tmp.ether_addr_octet, eth->ether_dhost, sizeof(eth->ether_dhost));
-		update(mac_mp, tmp, true, header->caplen); //recv ethernet packet
-		memcpy(tmp.ether_addr_octet, eth->ether_shost, sizeof(eth->ether_shost));
-		update(mac_mp, tmp, false, header->caplen); //send ethernet packet
+		ether_addr dst_eth{}, src_eth{};
+		memcpy(dst_eth.ether_addr_octet, eth->ether_dhost, sizeof(eth->ether_dhost));
+		memcpy(src_eth.ether_addr_octet, eth->ether_shost, sizeof(eth->ether_shost));
+
+		//cout << ether_ntoa(&src_eth) << " " << ether_ntoa(&dst_eth) << "\n";
+
+		update(mac_mp, src_eth, false, header->caplen); //send ethernet packet
+		update(mac_mp, dst_eth, true, header->caplen); //recv ethernet packet
+
+		flow_update(conv_ether, src_eth, dst_eth, header->caplen);
 
 		ip *_ip = (ip *) (packet + 14);
 		if (ntohs(eth->ether_type) == ETHERTYPE_IP) {
-			update(mp, _ip->ip_dst.s_addr, true, header->caplen); //recv ip packet
-			update(mp, _ip->ip_src.s_addr, false, header->caplen); //send ip packet
+			auto src_ip = _ip->ip_src.s_addr, dst_ip = _ip->ip_dst.s_addr;
+			update(mp, src_ip, true, header->caplen); //recv ip packet
+			update(mp, dst_ip, false, header->caplen); //send ip packet
+
+			if (_ip->ip_p == IPPROTO_TCP) {
+				auto *tcp = (tcphdr *) (packet + 14 + _ip->ip_hl * 4);
+				auto src_port = tcp->th_sport, dst_port = tcp->th_dport;
+
+				flow_update(conv_tcp, tcp_key(src_ip, src_port), tcp_key(dst_ip, dst_port), header->caplen);
+			}
 		}
 	}
 
@@ -93,7 +115,35 @@ int main(int argc, char *argv[]) {
 		     << " ethernet packets ("
 		     << info.rB << " bytes)\n "
 		     << "sent " << info.send << " ethernet packets (" << info.sB << " bytes)\n\n";
+	{
+		map<tcp_key, map<tcp_key, bool>> vis;
+		cout << "=== DUMP TCP Conversations ===\n";
+		cout << "A -> B     Send(Count/Len) / Recv(Count/Len)\n";
+		for (const auto &[a, b] : conv_tcp)
+			for (const auto &[c, d] : b) {
+				if (vis[a][c]) continue;
+				vis[c][a] = true;
+				auto rev = conv_tcp[c][a];
+				cout << inet_ntoa({a.first}) << ":" << ntohs(a.second) << " -> " << inet_ntoa({c.first}) << ":"
+				     << ntohs(c.second)
+				     << "    >    " << d.first << " packets " << d.second << " bytes / " << rev.first << " packets "
+				     << rev.second << " bytes\n";
+			}
+	}
 
+	{
+		map<ether_addr, map<ether_addr, bool>> vis;
+		cout << "\n=== DUMP Ethernet Conversations ===\n";
+		cout << "A -> B     Send(Count/Len) / Recv(Count/Len)\n";
+		for (const auto &[a, b] : conv_ether)
+			for (const auto &[c, d] : b) {
+				if (vis[a][c]) continue;
+				vis[c][a] = true;
+				auto rev = conv_ether[c][a];
+				cout << ether_ntoa(&a) << " -> " << ether_ntoa(&c) << "    >    " << d.first << " packets " << d.second
+				     << " bytes / " << rev.first << " packets " << rev.second << " bytes\n";
+			}
+	}
 
 	pcap_close(handle);
 }
